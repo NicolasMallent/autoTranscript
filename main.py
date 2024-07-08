16cd95ddb1d062754b37695433213f0df3a671f5
Nimall <43858694+NicolasMallent@users.noreply.github.com>
2024-07-08 09:18:18 +0200

    feat: Fix output file's name & remove hardcoded path

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import datetime
 import whisper
 import os
+import sys
 
-def floatToTime( time_float : float):
+def floatToTime(time_float: float):
     # Convert start time into a format [HH:MM:SS]
     delta = datetime.timedelta(seconds=time_float)
     hours, remainder = divmod(delta.total_seconds(), 3600)
@@ -10,22 +11,25 @@ def floatToTime( time_float : float):
     time_format = f"{int(hours):02}:{int(minutes):02}:{seconds:06.3f}"
     return time_format
 
+
 if __name__ == '__main__':
     if "ffmpeg" not in os.environ["PATH"]:
-        os.environ["PATH"] += os.pathsep +"C:\\Program Files\\ffmpeg-master-latest-win64-gpl\\bin"
+        os.environ["PATH"] += os.pathsep + "C:\\Program Files\\ffmpeg-master-latest-win64-gpl\\bin"
 
     # Chemin du dossier où se trouve les fichiers audio : les \ deviennent des \\
-    dossier = ("C:\\Users\\lirdef\\Desktop\\BTC\\Mtp\\texte-carto")
+    dossier = sys.argv[1]
+    print("Le dossier des fichiers audio est:", dossier)
 
     # Récupération des fichiers audio qui sont dans le dossier
     fichiers_audio = [f for f in os.listdir(dossier) if os.path.isfile(os.path.join(dossier, f))]
 
     # Modèle de vocabulaire : base, medium, large ; sera téléchargé une seule fois
-    vocabulaire = "large"
+    vocabulaire = sys.argv[2]
+    print("Le modèle de vocabulaire utilisé est:", vocabulaire)
     model = whisper.load_model(vocabulaire)
 
     # Activer l’option pour obtenir des détails, y compris les timings
-    options = { "task": "transcribe", "verbose": True, "language": "French" }
+    options = {"task": "transcribe", "verbose": True, "language": "French"}
 
     # Pour chaque fichier audio faire la transcription
     for fichier_audio in fichiers_audio:
@@ -33,8 +37,10 @@ if __name__ == '__main__':
         chemin_fichier_audio = os.path.join(dossier, fichier_audio)
 
         # Le fichier de sorti (format texte) est sauvegardé à côté des fichiers audio (dans le même dossier)
-        fichier_sorti = os.path.join(dossier, "output_" + chemin_fichier_audio.split("\\")[-1] + "" + vocabulaire + ".txt")
-        
+        filename_without_extension = os.path.basename(chemin_fichier_audio).split(".")[0]
+        fichier_sorti = os.path.join(dossier,
+                                     "output_" + filename_without_extension + "_" + vocabulaire + ".txt")
+
         # Transcription
         result = model.transcribe(chemin_fichier_audio, **options)
 
@@ -43,4 +49,6 @@ if __name__ == '__main__':
             for segment in result["segments"]:
                 start_time = floatToTime(segment['start'])
                 end_time = floatToTime(segment['end'])
-                fichier_output_txt.write("\n[" + start_time + " --> " + end_time + "]" + segment['text'].replace("... ", "...\n").replace(". ", ".\n").replace("! ", "!\n").replace("? ", "?\n"))
+                fichier_output_txt.write(
+                    "\n[" + start_time + " --> " + end_time + "]" + segment['text'].replace("... ", "...\n").replace(
+                        ". ", ".\n").replace("! ", "!\n").replace("? ", "?\n"))
